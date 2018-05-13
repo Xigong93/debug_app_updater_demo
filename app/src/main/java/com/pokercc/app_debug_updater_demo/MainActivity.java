81be7407a545c64a8d6484827d4268e6e81beb4c
pokercc <pokercc@sina.com>
2018-05-13 23:46:06 +0800

    init

---
+++ app/src/main/java/com/pokercc/app_debug_updater_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pokercc.app_debug_updater_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
